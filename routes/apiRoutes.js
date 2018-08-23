66b258027d6c297a94d1af2e0ca52d22505dbd25
zane <neuschuler@gmail.com>
2018-08-23 14:53:21 -0700

    adding giantbomb api call

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,6 @@
 var db = require("../models");
+var giantbomb = require("giantbomb");
+var gb = giantbomb(process.env.GB);
 
 module.exports = function(app) {
   // Get all examples
@@ -20,5 +22,28 @@ module.exports = function(app) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
     });
+  });
+  app.get("/api/giantbomb/:game", function (req, res) {
+    var response = [];
+    console.log(req.params.game);
+    gb.games.search(req.params.game, (err, rez, json) => {
+      if (err) throw err;
+      let games = json.results;
+        games.forEach(game => {
+          debugger;
+          let newGame = {
+            gameName: game.name,
+            gameBio: game.deck,
+            gamePicture: game.image.original_url,
+            giantbombID: game.guid,
+            gbURL: game.site_detail_url
+          };
+          response.push(newGame);
+          console.log(response);
+
+        });
+        res.json(response);
+    });
+
   });
 };
